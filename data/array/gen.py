2ff971eccece14be740789298ea91b7b612c3bba
dantle1 <dantle@ucsb.edu>
2024-03-09 20:24:08 +0000

    Dan - added merge and quicksort in cpp

--- data/array/gen.py
+++ data/array/gen.py
@@ -4,8 +4,8 @@ import sys
 if __name__ == '__main__':
     f = open('array.in', 'w')
     n = int(sys.stdin.readline())
-
-    A = np.random.randint(1, 2**63 - 1, (1, n))
+    f.write(str(n) + '\n')
+    A = np.random.randint(1, 10, (1, n))
     np.savetxt(f, A, fmt="%d", delimiter=' ')
 
     f.close()
